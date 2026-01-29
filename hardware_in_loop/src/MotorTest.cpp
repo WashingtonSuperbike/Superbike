d67508ac3e29cc57fe68d167b5497587991e77b5
Tanmay <tanmay.x@hotmail.com>
2026-01-29 13:37:11 -0800

    added full set of faults

--- hardware_in_loop/src/MotorTest.cpp
+++ hardware_in_loop/src/MotorTest.cpp
@@ -6,12 +6,20 @@
 #define MOTOR_TEMPS_MSG 0x0CF11F05
  
 // MOTOR_STATS errors
+//Byte[6]:
 #define IDENTIFICATION_FAULT 0x01
 #define HIGH_VOLTAGE_FAULT   0x02
 #define LOW_VOLTAGE_FAULT    0x04
 #define STALL_FAULT          0x10
 #define INTERNAL_VOLTS_FAULT 0x20
-#define OVER_TEMPERATURE_FAULT 0x40
+#define CONTROLLER_OVER_TEMPERATURE_FAULT 0x40
+#define THROTTLE_SIGNAL_FAULT      0x80
+//Byte[7]:
+#define INTERNAL_RESET_FAULT   0x02
+#define THROTTLE_CIRCUIT_FAULT 0x04
+#define ANGLE_SENSOR_FAULT   0x08
+#define MOTOR_OVER_TEMPERATURE_FAULT 0x40
+#define GALVANOMETER_FAULT        0x80
  
 // Instantiate the CAN object for CAN2
 FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> CAN_2;
@@ -160,7 +168,73 @@ void sendOverTemperatureFault() {
   msg.id = MOTOR_STATS_MSG;
   msg.len = 8;
   for(int i=0; i<8; i++) msg.buf[i] = 0;
-  msg.buf[6] = OVER_TEMPERATURE_FAULT;
+  msg.buf[6] = CONTROLLER_OVER_TEMPERATURE_FAULT;
   CAN_2.write(msg);
-  Serial.println("Sent: Over Temp Fault");
+  Serial.println("Sent: Controller Over Temperature Fault");
+}
+
+void sendThrottleSignalFault() {
+  CAN_message_t msg;
+  msg.flags.extended = 1;
+  msg.id = MOTOR_STATS_MSG;
+  msg.len = 8;
+  for(int i=0; i<8; i++) msg.buf[i] = 0;
+  msg.buf[6] = THROTTLE_SIGNAL_FAULT;
+  CAN_2.write(msg);
+  Serial.println("Sent: Throttle SignalFault");
+}
+
+void sendInternalResetFault() {
+  CAN_message_t msg;
+  msg.flags.extended = 1;
+  msg.id = MOTOR_STATS_MSG;
+  msg.len = 8;
+  for(int i=0; i<8; i++) msg.buf[i] = 0;
+  msg.buf[7] = INTERNAL_RESET_FAULT;
+  CAN_2.write(msg);
+  Serial.println("Sent: Internal Reset Fault");
+}
+
+void sendThrottleCircuitFault() {
+  CAN_message_t msg;
+  msg.flags.extended = 1;
+  msg.id = MOTOR_STATS_MSG;
+  msg.len = 8;
+  for(int i=0; i<8; i++) msg.buf[i] = 0;
+  msg.buf[7] = THROTTLE_CIRCUIT_FAULT;
+  CAN_2.write(msg);
+  Serial.println("Sent: Throttle Circuit Fault");
+}
+
+void sendAngleSensorFault() {
+  CAN_message_t msg;
+  msg.flags.extended = 1;
+  msg.id = MOTOR_STATS_MSG;
+  msg.len = 8;
+  for(int i=0; i<8; i++) msg.buf[i] = 0;
+  msg.buf[7] = ANGLE_SENSOR_FAULT;
+  CAN_2.write(msg);
+  Serial.println("Sent: Angle Sensor Fault");
+}
+
+void motorOverTemperatureFault() {
+  CAN_message_t msg;
+  msg.flags.extended = 1;
+  msg.id = MOTOR_STATS_MSG;
+  msg.len = 8;
+  for(int i=0; i<8; i++) msg.buf[i] = 0;
+  msg.buf[7] = MOTOR_OVER_TEMPERATURE_FAULT;
+  CAN_2.write(msg);
+  Serial.println("Sent: Motor Over Temperature Fault");
+}
+
+void sendGalvanometerFault() {
+  CAN_message_t msg;
+  msg.flags.extended = 1;
+  msg.id = MOTOR_STATS_MSG;
+  msg.len = 8;
+  for(int i=0; i<8; i++) msg.buf[i] = 0;
+  msg.buf[7] = GALVANOMETER_FAULT;
+  CAN_2.write(msg);
+  Serial.println("Sent: Galvanometer Fault");
 }
